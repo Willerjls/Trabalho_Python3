04379becaea634dcca487db21905cc80837fa8f9
Willerjls <willer.jacques555@gmail.com>
2020-11-19 08:22:15 -0300

    Add_Authentication

--- Tema1/tema1/views.py
+++ Tema1/tema1/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from tema1.models import Curso, Professor, Disciplina
 from tema1.serializer import CursoSerializer, ProfessorSerializer, DisciplinaSerializer
 
@@ -6,13 +8,19 @@ from tema1.serializer import CursoSerializer, ProfessorSerializer, DisciplinaSer
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class ProfessorViewSet(viewsets.ModelViewSet):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class DisciplinaViewSet(viewsets.ModelViewSet):
     queryset = Disciplina.objects.all()
     serializer_class = DisciplinaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
